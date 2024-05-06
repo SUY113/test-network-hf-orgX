523e8e8ce1f1524f86fa96b4978f31993d7b3e0e
SUY113 <tntuan110322@gmail.com>
2024-05-06 16:30:37 +0700

    Update scipt createChannel

--- scripts/createChannel.sh
+++ scripts/createChannel.sh
@@ -26,7 +26,7 @@ createChannelTx() {
 }
 
 createChannel() {
-	setGlobals 1
+	setGlobals Staff
 	# Poll in case the raft leader is not set yet
 	local rc=1
 	local COUNTER=1
@@ -84,15 +84,15 @@ createChannel
 successln "Channel '$CHANNEL_NAME' created"
 
 ## Join all the peers to the channel
-infoln "Joining org1 peer to the channel..."
-joinChannel 1
+infoln "Joining orgStaff peer to the channel..."
+joinChannel Staff
 infoln "Joining org2 peer to the channel..."
-joinChannel 2
+joinChannel Accountant
 
 ## Set the anchor peers for each org in the channel
-infoln "Setting anchor peer for org1..."
-setAnchorPeer 1
-infoln "Setting anchor peer for org2..."
-setAnchorPeer 2
+infoln "Setting anchor peer for orgStaff..."
+setAnchorPeer Staff
+infoln "Setting anchor peer for orgAccountant..."
+setAnchorPeer Accountant
 
 successln "Channel '$CHANNEL_NAME' joined"
